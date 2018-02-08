2496c050df07a719dc9e14ac0d268d783cde906d
Christopher Williams <christopher.brett716@gmail.com>
2018-02-08 18:57:45 -0500

    media greeter

--- media/greeting.js
+++ media/greeting.js
@@ -1,3 +1,3 @@
-var name = ("hi there! what's your name?");
+var name = prompt("hi there! what's your name?");
 var output = document.querySelector('#greeting');
-output.innerHTML = "<p>thanks for visiting " + "name" + ".</p>"
+output.innerHTML = "<p>thanks for visiting, " + name + ".</p>"
