d1d57f1974a4480b4daa46b6197a904b661482b0
Christopher Williams <christopher.brett716@gmail.com>
2018-02-22 20:47:41 -0500

    Main page js// game button

---
+++ main.js
@@ -0,0 +1,4 @@
+var contactArray = ["Raindrop , drop top", "Chris Williams", "1503 East 14th Street", "Chattanooga", "TN", "37404", "712.395.8019", "christopher.brett716@gmail.com", "https://twitter.com/_jennydeath"];
+
+var extraContact = contactArray.pop();
+var dailyInspo = contactArray.shift();
